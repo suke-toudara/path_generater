e39f2d2f5263ffabe29ccff56742ad5cc6373468
keisuke yoshida <82552894+suke-toudara@users.noreply.github.com>
2024-10-23 11:08:30 +0900

    Update simple_path_maker.cpp

--- src/simple_path_maker.cpp
+++ src/simple_path_maker.cpp
@@ -1,10 +1,40 @@
-#include <cstdio>
+#ifndef PATH_NODE_HPP
+#define WAYPOINT_PATH_NODE_HPP
 
-int main(int argc, char ** argv)
+#include <rclcpp/rclcpp.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <nav_msgs/msg/path.hpp>
+#include <vector>
+
+enum class InterpolationMethod
 {
-  (void) argc;
-  (void) argv;
+    LINEAR,
+    BEZIER,
+    SPLINE
+};
 
-  printf("hello world simple_path_maker package\n");
-  return 0;
-}
+class WaypointPathNode : public rclcpp::Node
+{
+public:
+    WaypointPathNode();
+    
+    // メソッドの選択を設定
+    void set_interpolation_method(InterpolationMethod method);
+
+private:
+    void waypoint_callback(const geometry_msgs::msg::PoseStamped::SharedPtr msg);
+    void generate_path();
+    
+    // 補完方法ごとの処理
+    void linear_interpolation(const geometry_msgs::msg::PoseStamped &start, const geometry_msgs::msg::PoseStamped &end, nav_msgs::msg::Path &path);
+    void bezier_interpolation(const std::vector<geometry_msgs::msg::PoseStamped> &control_points, nav_msgs::msg::Path &path);
+    void spline_interpolation(const std::vector<geometry_msgs::msg::PoseStamped> &points, nav_msgs::msg::Path &path);
+
+    rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr waypoint_subscription_;
+    rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr path_publisher_;
+    
+    std::vector<geometry_msgs::msg::PoseStamped> waypoints_;
+    InterpolationMethod interpolation_method_;
+};
+
+#endif // WAYPOINT_PATH_NODE_HPP
