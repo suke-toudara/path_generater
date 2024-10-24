fcd17df5369206bbfefd62583d247467204655c8
keisuke yoshida <82552894+suke-toudara@users.noreply.github.com>
2024-10-24 17:21:46 +0900

    Update path_generater.hpp

--- include/simple_path_generater/path_generater.hpp
+++ include/simple_path_generater/path_generater.hpp
@@ -1,5 +1,5 @@
-#ifndef WAYPOINT_PATH_NODE_HPP
-#define WAYPOINT_PATH_NODE_HPP
+#ifndef PATH_GENERATER_HPP
+#define PATH_GENERATER_HPP
 
 #include <rclcpp/rclcpp.hpp>
 #include <geometry_msgs/msg/pose_stamped.hpp>
@@ -13,7 +13,6 @@ class PathGenerater : public rclcpp::Node
 {
 public:
     PathGenerater();
-    void set_interpolation_method(InterpolationMethod method);
 
 private:
     void waypoint_cb(const geometry_msgs::msg::PoseStamped & waypoints);
@@ -21,10 +20,7 @@ private:
     
     rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr waypoint_sub_;
     rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr path_pub_;
-    
-    SimplelinePath sim_line_path;
-
 };
 } // namespace path_generater
 
-#endif // WAYPOINT_PATH_NODE_HPP
+#endif // PATH_GENERATER_HPP
